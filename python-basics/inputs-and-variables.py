f35727d4574dde82d2107ef17632f2125255b090
EddyKaggia <kagzcodes@gmail.com>
2024-03-06 11:26:50 -0800

    feat: first commit

---
+++ python-basics/inputs-and-variables.py
@@ -0,0 +1,22 @@
+# Creating variables
+myName = input("What's your name?")
+myAge = input("How old are you?")
+print("Lord, you're old!")
+python = input("Do you like python?")
+print("I really do!")
+print()
+print("So, you are")
+print(myName)
+print("and are the ripe old age of")
+print(myAge)
+print("and clearly think that Python is")
+print(python)
+
+
+# Challenge
+print("Getting to know you!")
+
+yourName = input("What's your name?")
+favoriteFood = input("What's your favorite food?")
+favoriteMusic = input("What's your favorite music?")
+yourState = input("Where do you live?")
---
+++ python-basics/print.py
@@ -0,0 +1,9 @@
+# Print logs statement in parenthesis to the console
+print("Hello Replit")
+
+# Printing 3 sets of quotes
+print("""
+Here's an exact
+  Bit
+    Of
+      Text""" )
