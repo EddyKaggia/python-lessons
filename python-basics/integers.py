77fb657ffa6d6ab9475b6411146e6f0b782d4bb0
EddyKaggia <kagzcodes@gmail.com>
2024-03-26 09:29:05 -0700

    feat: integers

---
+++ python-basics/integers.py
@@ -0,0 +1,21 @@
+# INTEGERS
+
+# Underscores in Numbers
+# 1000 is the same as 1_000, which is the same as 10_00
+
+universe_age = 14_000_000_000
+print(universe_age) # 14000000000
+
+# Multiple Assignment
+
+x, y, z = 0, 2, 5
+
+print(x) # 0
+print(y) # 2
+print(z) # 5
+
+# Constants
+# Use all capital letters to indicate it's a constant
+
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
